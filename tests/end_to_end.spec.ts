0b89fc4a0186cfc68328c202598e254cca2793bb
jennyrmdixon <45156191+jennyrmdixon@users.noreply.github.com>
2025-08-11 21:23:12 -0700

    Remove unncessary comments

--- tests/end_to_end.spec.ts
+++ tests/end_to_end.spec.ts
@@ -35,7 +35,7 @@ test.describe('E2E Checkout Test', () => {
 
   test('End to End Test', async ({ page }) => {
     test.setTimeout(120000) // 2m overall for test time
-    page.setDefaultTimeout(30000); // 30 s for all locator actions & waits
+    page.setDefaultTimeout(30000); // 30 s for all locator actions & waits. Does not apply to expects
 
     // Define frequently used locator
     const LOCATOR_PRODUCT_NAME = page.locator('[data-test="product-name"]');
@@ -51,7 +51,7 @@ test.describe('E2E Checkout Test', () => {
 
       await Promise.all([
         page.locator('[data-test="search-submit"]').click(),
-        // Extended wait to this step - search may take up to 1 minute to complete
+        // Slow step - search may take up to 1 minute to complete
         page.waitForSelector('[data-test="search_completed"]', { state: 'visible', timeout: 60000 })
       ])
       const PRODUCT_1_CARD = page.locator('[data-test^="product-"]').first();
@@ -61,8 +61,8 @@ test.describe('E2E Checkout Test', () => {
 
       // Click on first product, confirm product details
       await PRODUCT_1_CARD.click();
-      await LOCATOR_PRODUCT_NAME.waitFor({ state: 'visible' }), 
-      await expect(LOCATOR_PRODUCT_NAME).toHaveText(item_1.name as string);
+      await LOCATOR_PRODUCT_NAME.waitFor({ state: 'visible' }),
+        await expect(LOCATOR_PRODUCT_NAME).toHaveText(item_1.name as string);
       await expect(page.locator('[data-test="unit-price"]')).toHaveText(new RegExp(item_1.price as string));
     }); // End step
 
@@ -89,11 +89,11 @@ test.describe('E2E Checkout Test', () => {
       await Promise.all([
         page.locator('[data-test="add-to-cart"]').click(),
         page.waitForSelector('[data-test="cart-quantity"]', { state: 'visible' }),
-      ]);      
+      ]);
       // Validate cart displays 1 item added
       await expect(page.locator('[data-test="cart-quantity"]')).toHaveText("1");
     }); // End step
-    
+
 
     await test.step(`Navigate to third product, add to cart`, async () => {
 
@@ -110,118 +110,111 @@ test.describe('E2E Checkout Test', () => {
       item_3.price = normalizeText(item_3.price);
 
       // Click on third product
-        await PRODUCT_3_CARD.click(),
-     
-      // FLAKY STEP DUE TO SLOW LOADING - TRY EXTENDING  STEP 
-      await expect(LOCATOR_PRODUCT_NAME).toHaveText(item_3.name as string);
+      await PRODUCT_3_CARD.click(),
+        await expect(LOCATOR_PRODUCT_NAME).toHaveText(item_3.name as string);
       await expect(page.locator('[data-test="unit-price"]')).toHaveText(new RegExp(item_3.price as string));
 
-         // Add current item to cart
-         // Clean up this section to make sure the 
+      // Add current item to cart
       await Promise.all([
         page.locator('[data-test="add-to-cart"]').click(),
         page.waitForSelector('[data-test="cart-quantity"]', { state: 'visible' }),
-      ]);      
-      // Validate cart displays 2 items added - remove timeout??
+      ]);
       await expect(page.locator('[data-test="cart-quantity"]')).toHaveText("2");
-    // End step
+      // End step
 
       await test.step(`Navigate to cart, check details, and increase item quantity`, async () => {
-      await page.locator('[data-test="nav-cart"]').click();
-      // Validate 2 items are present, prices match what was gotten from home page
-      // Determine expected display order of products, based on alpha order
-      let item2NameNorm = normalizeText(item_2.name);
-      let item3NameNorm = normalizeText(item_3.name);
+        await page.locator('[data-test="nav-cart"]').click();
+        // Validate 2 items are present, prices match what was captured from home page
+        // Determine expected display order of products, based on alpha order
+        let item2NameNorm = normalizeText(item_2.name);
+        let item3NameNorm = normalizeText(item_3.name);
 
-      let item2Order;
-      let item3Order;
+        let item2Order;
+        let item3Order;
 
-      if (item2NameNorm < item3NameNorm) {
-        item2Order = 0;
-        item3Order = 1;
-      }
-      else if (item3NameNorm > item2NameNorm) {
-        item3Order = 0;
-        item2Order = 1;
-      }
-      else {
-        console.log("error")
-      }
-
-      // Check details of item 2
-      await expect((page.locator('[data-test="product-title"]').nth(item2Order))).toHaveText(item2NameNorm);
-      const item2Price = normalizeText(await page.locator('[data-test="product-price"]').nth(item2Order).textContent());
-      expect(item2Price).toBe(item_2.price);
-
-      // Check details of item 3
-      await expect((page.locator('[data-test="product-title"]').nth(item3Order))).toHaveText(item3NameNorm);
-      const item3Price = normalizeText(await page.locator('[data-test="product-price"]').nth(item3Order).textContent());
-      expect(item3Price).toBe(item_3.price);
-
-      let cartTotal = (parseFloat(item2Price) + parseFloat(item3Price)).toString();
-      let normalizedTotal = normalizeText(await page.locator('[data-test="cart-total"]').textContent());
-      expect(normalizedTotal).toEqual(cartTotal);
-
-      // Increase quanity of item 2 from 1 to 2
-      await page.getByRole('spinbutton').nth(item2Order).click();
-      await page.getByRole('spinbutton').nth(item2Order).fill('2');
-      // De-select item counter box
-      await page.locator("body").click({ position: { x: 0, y: 0 } });
-
-      let newExpectedTotal = parseFloat(cartTotal) + parseFloat(item2Price);
-      await expect.poll(async () => {
-        try {
-          const newTotal = normalizeText(await page.locator('[data-test="cart-total"]').textContent());
-          const newTotalParse = parseFloat(newTotal);
-          return newTotalParse;
-        } catch {
-          return false;
+        if (item2NameNorm < item3NameNorm) {
+          item2Order = 0;
+          item3Order = 1;
         }
-      }
-      ).toBe(newExpectedTotal)
-    }); // End step
+        else if (item3NameNorm > item2NameNorm) {
+          item3Order = 0;
+          item2Order = 1;
+        }
+        else {
+          console.log("error")
+        }
+
+        // Check details of item 2
+        await expect((page.locator('[data-test="product-title"]').nth(item2Order))).toHaveText(item2NameNorm);
+        const item2Price = normalizeText(await page.locator('[data-test="product-price"]').nth(item2Order).textContent());
+        expect(item2Price).toBe(item_2.price);
+
+        // Check details of item 3
+        await expect((page.locator('[data-test="product-title"]').nth(item3Order))).toHaveText(item3NameNorm);
+        const item3Price = normalizeText(await page.locator('[data-test="product-price"]').nth(item3Order).textContent());
+        expect(item3Price).toBe(item_3.price);
+
+        let cartTotal = (parseFloat(item2Price) + parseFloat(item3Price)).toString();
+        let normalizedTotal = normalizeText(await page.locator('[data-test="cart-total"]').textContent());
+        expect(normalizedTotal).toEqual(cartTotal);
+
+        // Increase quanity of item 2 from 1 to 2
+        await page.getByRole('spinbutton').nth(item2Order).click();
+        await page.getByRole('spinbutton').nth(item2Order).fill('2');
+        // De-select item counter box
+        await page.locator("body").click({ position: { x: 0, y: 0 } });
+
+        let newExpectedTotal = parseFloat(cartTotal) + parseFloat(item2Price);
+        await expect.poll(async () => {
+          try {
+            const newTotal = normalizeText(await page.locator('[data-test="cart-total"]').textContent());
+            const newTotalParse = parseFloat(newTotal);
+            return newTotalParse;
+          } catch {
+            return false;
+          }
+        }
+        ).toBe(newExpectedTotal)
+      }); // End step
 
 
-      // EDIT TO WAIT FOR EACH PROCEED
       await test.step(`Complete checkout`, async () => {
-      await page.locator('[data-test="proceed-1"]').click();
-      // await page.locator('[data-test="email"]').click();
-      await page.locator('[data-test="email"]').click();
-      await page.locator('[data-test="email"]').fill('customer2@practicesoftwaretesting.com');
-      await page.locator('[data-test="password"]').click();
-      await page.locator('[data-test="password"]').fill('welcome01');
-      await page.locator('[data-test="login-submit"]').click();
-      await page.locator('[data-test="proceed-2"]').click();
-      // Assumes default customer already has street, city and country filled out in
-      // Wait for city to populate and display text from customer account
-      await expect.poll(async () => {
-        const city = await page.locator('[data-test="city"]').inputValue();
-        return city && city.trim().length > 0;
-      }).toBe(true);
-      // await expect(page.locator('[data-test="city"]')).toHaveText(/.+/);
-      await page.locator('[data-test="street"]').click();
-      await page.locator('[data-test="state"]').fill('Burgenland');
-      // FLAKY STEP (both click and fill)
-      await page.locator('[data-test="postal_code"]').click();
-      await page.locator('[data-test="postal_code"]').fill('7461');
-      // FLAKY STEP (click and fill)
-      await page.locator('[data-test="proceed-3"]').click();
-      await page.locator('[data-test="payment-method"]').selectOption('credit-card');
-      await page.locator('[data-test="credit_card_number"]').click();
-      await page.locator('[data-test="credit_card_number"]').fill('0000-0000-0000-0000');
-      await page.locator('[data-test="expiration_date"]').click();
-      await page.locator('[data-test="expiration_date"]').click();
-      await page.locator('[data-test="expiration_date"]').fill('12/2030');
-      await page.locator('[data-test="cvv"]').click();
-      await page.locator('[data-test="cvv"]').fill('123');
-      await page.locator('[data-test="card_holder_name"]').click();
-      await page.locator('[data-test="card_holder_name"]').fill('Jack Howe');
-      await page.locator('[data-test="finish"]').click();
-      // FLAKY STEP
-      await expect(page.locator('[data-test="payment-success-message"]')).toBeVisible();
+        // Checkout page 1
+        await page.locator('[data-test="proceed-1"]').click();
+        await page.locator('[data-test="email"]').click();
+        await page.locator('[data-test="email"]').fill('customer2@practicesoftwaretesting.com');
+        await page.locator('[data-test="password"]').click();
+        await page.locator('[data-test="password"]').fill('welcome01');
+        await page.locator('[data-test="login-submit"]').click();
+        // Checkout page 2
+        await page.locator('[data-test="proceed-2"]').click();
+        // Assumes default customer already has street, city and country filled out in
+        // Wait for city to populate and display text from customer account
+        await expect.poll(async () => {
+          const city = await page.locator('[data-test="city"]').inputValue();
+          return city && city.trim().length > 0;
+        }).toBe(true);
+        await page.locator('[data-test="street"]').click();
+        await page.locator('[data-test="state"]').fill('Burgenland');
+        await page.locator('[data-test="postal_code"]').click();
+        await page.locator('[data-test="postal_code"]').fill('7461');
+        // Checkout page 3
+        await page.locator('[data-test="proceed-3"]').click();
+        await page.locator('[data-test="payment-method"]').selectOption('credit-card');
+        await page.locator('[data-test="credit_card_number"]').click();
+        await page.locator('[data-test="credit_card_number"]').fill('0000-0000-0000-0000');
+        await page.locator('[data-test="expiration_date"]').click();
+        await page.locator('[data-test="expiration_date"]').click();
+        await page.locator('[data-test="expiration_date"]').fill('12/2030');
+        await page.locator('[data-test="cvv"]').click();
+        await page.locator('[data-test="cvv"]').fill('123');
+        await page.locator('[data-test="card_holder_name"]').click();
+        await page.locator('[data-test="card_holder_name"]').fill('Jack Howe');
+        await page.locator('[data-test="finish"]').click();
+        await expect(page.locator('[data-test="payment-success-message"]')).toBeVisible();
+      });
     });
-  });
 
-});
+  });
 });
 
