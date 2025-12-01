8f29e87b55c59ff494fe2d73c19d14148a7abb7c
jennyrmdixon <45156191+jennyrmdixon@users.noreply.github.com>
2025-12-01 06:31:01 -0800

    Add tsconfig file

--- tests/billing_address.spec.ts
+++ tests/billing_address.spec.ts
@@ -36,97 +36,97 @@ test.describe('Billing Address Test', () => {
 
     // Helper function to set preconditions for various flows
 
-async function setPreconditions(testCase: string, page: Page,) {
-    // Begin on home page
-    await page.goto('https://practicesoftwaretesting.com/');
-    await expect(page).toHaveURL('https://practicesoftwaretesting.com/');
-    await expect(page.locator('[data-test="nav-categories"]')).toMatchAriaSnapshot(`- button "Categories"`);
-    await page.locator('[data-test="nav-categories"]').click();
-    await page.locator('[data-test="nav-hand-tools"]').click();
-    await page.locator('[data-test^="product-"]').first().click();
-    await page.locator('[data-test="add-to-cart"]').click();
-    await page.locator('[data-test="nav-cart"]').click();
-    await page.locator('[data-test="proceed-1"]').click();
+    async function setPreconditions(testCase: string, page: Page,) {
+        // Begin on home page
+        await page.goto('https://practicesoftwaretesting.com/');
+        await expect(page).toHaveURL('https://practicesoftwaretesting.com/');
+        await expect(page.locator('[data-test="nav-categories"]')).toMatchAriaSnapshot(`- button "Categories"`);
+        await page.locator('[data-test="nav-categories"]').click();
+        await page.locator('[data-test="nav-hand-tools"]').click();
+        await page.locator('[data-test^="product-"]').first().click();
+        await page.locator('[data-test="add-to-cart"]').click();
+        await page.locator('[data-test="nav-cart"]').click();
+        await page.locator('[data-test="proceed-1"]').click();
 
-    if (testCase === "noAccount"){
-        const GUEST = {
-            firstName: "Jack",
-            lastName: "Howe",
-            email: "customer101@practicesoftwaretesting.com"
+        if (testCase === "noAccount") {
+            const GUEST = {
+                firstName: "Jack",
+                lastName: "Howe",
+                email: "customer101@practicesoftwaretesting.com"
+            }
+            await page.locator('a[href="#guest-tab"]').click();
+            await page.locator('[data-test="guest-first-name"]').fill(GUEST.firstName)
+            await page.locator('[data-test="guest-last-name"]').fill(GUEST.lastName)
+            await page.locator('[data-test="guest-email"]').fill(GUEST.email)
+            await page.locator('[data-test="guest-submit"]').click();
+            await page.locator('[data-test="proceed-2-guest"]').click();
         }
-        await page.locator('a[href="#guest-tab"]').click();
-        await page.locator('[data-test="guest-first-name"]').fill(GUEST.firstName)
-        await page.locator('[data-test="guest-last-name"]').fill(GUEST.lastName)
-        await page.locator('[data-test="guest-email"]').fill(GUEST.email)
-        await page.locator('[data-test="guest-submit"]').click();
-        await page.locator('[data-test="proceed-2-guest"]').click();
+
     }
 
-}
-
-const BILLING_ADDRESS_FIELDS = {
-  street: '[data-test="street"]',
-  city: '[data-test="city"]',
-  state: '[data-test="state"]',
-  country: '[data-test="country"]',
-  postal: '[data-test="postal_code"]',
-}
-
-test('Guest Checkout Flow', async ({page}) => {
-
-    test.setTimeout(120000) // 2m overall for test time
-    await setPreconditions("noAccount", page);
-
-    async function validateProceedDisabled() {
-    let PROCEED_BUTTON = page.locator('[data-test="proceed-3"]');
-    await expect(PROCEED_BUTTON).toBeDisabled();
+    const BILLING_ADDRESS_FIELDS = {
+        street: '[data-test="street"]',
+        city: '[data-test="city"]',
+        state: '[data-test="state"]',
+        country: '[data-test="country"]',
+        postal: '[data-test="postal_code"]',
     }
 
-    await test.step("Confirm character limits for fields", async() => {
+    test('Guest Checkout Flow', async ({ page }) => {
 
-    const CHAR_TEXT_0 = "0";
-    // Postal/zip code field allows max 10 characters
-    const CHAR_TEXT_10 = "012345 AB-";
-    const CHAR_TEXT_11 = "012345 AB-C";
-    // Country, city, and state fields allow max 40 characters
-    const CHAR_TEXT_40 = "abc 124 ghi jkl mno pqrs tuv wxyzz AB. D";
-    const CHAR_TEXT_41 = "abc 124 ghi jkl mno pqrs tuv wxyzz AB. DC";
-    // Street field allows max 70 characters
-    const CHAR_TEXT_70 = "abc def ghi jkl mno pqr's tuv wxyz ABC DEF GHI JKL MNO PQRS TUV WXYZ !";
-    const CHAR_TEXT_71 = "abc def ghi jkl mno pqr's tuv wxyz ABC DEF GHI JKL MNO PQRS TUV WXYZ !§";
+        test.setTimeout(120000) // 2m overall for test time
+        await setPreconditions("noAccount", page);
 
-    // Only street field has invalid length
-    await page.locator(BILLING_ADDRESS_FIELDS.street).fill(CHAR_TEXT_71);
-    await page.locator(BILLING_ADDRESS_FIELDS.city).fill(CHAR_TEXT_40);
-    await page.locator(BILLING_ADDRESS_FIELDS.state).fill(CHAR_TEXT_40);
-    await page.locator(BILLING_ADDRESS_FIELDS.country).fill(CHAR_TEXT_40);
-    await page.locator(BILLING_ADDRESS_FIELDS.postal).fill(CHAR_TEXT_10);
-    validateProceedDisabled();
+        async function validateProceedDisabled() {
+            let PROCEED_BUTTON = page.locator('[data-test="proceed-3"]');
+            await expect(PROCEED_BUTTON).toBeDisabled();
+        }
 
-    // Only city field has invalid length
-    await page.locator(BILLING_ADDRESS_FIELDS.street).fill(CHAR_TEXT_70);
-    await page.locator(BILLING_ADDRESS_FIELDS.city).fill(CHAR_TEXT_41);
-    validateProceedDisabled();
-    
-    // Only state field has invalid length
-    await page.locator(BILLING_ADDRESS_FIELDS.city).fill(CHAR_TEXT_40);
-    await page.locator(BILLING_ADDRESS_FIELDS.state).fill(CHAR_TEXT_41);
-    validateProceedDisabled();
+        await test.step("Confirm character limits for fields", async () => {
 
-    // Only country field has invalid length
-    await page.locator(BILLING_ADDRESS_FIELDS.state).fill(CHAR_TEXT_40);
-    await page.locator(BILLING_ADDRESS_FIELDS.country).fill(CHAR_TEXT_41);
-    validateProceedDisabled();
+            const CHAR_TEXT_0 = "0";
+            // Postal/zip code field allows max 10 characters
+            const CHAR_TEXT_10 = "012345 AB-";
+            const CHAR_TEXT_11 = "012345 AB-C";
+            // Country, city, and state fields allow max 40 characters
+            const CHAR_TEXT_40 = "abc 124 ghi jkl mno pqrs tuv wxyzz AB. D";
+            const CHAR_TEXT_41 = "abc 124 ghi jkl mno pqrs tuv wxyzz AB. DC";
+            // Street field allows max 70 characters
+            const CHAR_TEXT_70 = "abc def ghi jkl mno pqr's tuv wxyz ABC DEF GHI JKL MNO PQRS TUV WXYZ !";
+            const CHAR_TEXT_71 = "abc def ghi jkl mno pqr's tuv wxyz ABC DEF GHI JKL MNO PQRS TUV WXYZ !§";
 
-    // Only zip field has invalid length
-    await page.locator(BILLING_ADDRESS_FIELDS.country).fill(CHAR_TEXT_40);
-    await page.locator(BILLING_ADDRESS_FIELDS.postal).fill(CHAR_TEXT_11);
-    validateProceedDisabled();
+            // Only street field has invalid length
+            await page.locator(BILLING_ADDRESS_FIELDS.street).fill(CHAR_TEXT_71);
+            await page.locator(BILLING_ADDRESS_FIELDS.city).fill(CHAR_TEXT_40);
+            await page.locator(BILLING_ADDRESS_FIELDS.state).fill(CHAR_TEXT_40);
+            await page.locator(BILLING_ADDRESS_FIELDS.country).fill(CHAR_TEXT_40);
+            await page.locator(BILLING_ADDRESS_FIELDS.postal).fill(CHAR_TEXT_10);
+            validateProceedDisabled();
+
+            // Only city field has invalid length
+            await page.locator(BILLING_ADDRESS_FIELDS.street).fill(CHAR_TEXT_70);
+            await page.locator(BILLING_ADDRESS_FIELDS.city).fill(CHAR_TEXT_41);
+            validateProceedDisabled();
+
+            // Only state field has invalid length
+            await page.locator(BILLING_ADDRESS_FIELDS.city).fill(CHAR_TEXT_40);
+            await page.locator(BILLING_ADDRESS_FIELDS.state).fill(CHAR_TEXT_41);
+            validateProceedDisabled();
+
+            // Only country field has invalid length
+            await page.locator(BILLING_ADDRESS_FIELDS.state).fill(CHAR_TEXT_40);
+            await page.locator(BILLING_ADDRESS_FIELDS.country).fill(CHAR_TEXT_41);
+            validateProceedDisabled();
+
+            // Only zip field has invalid length
+            await page.locator(BILLING_ADDRESS_FIELDS.country).fill(CHAR_TEXT_40);
+            await page.locator(BILLING_ADDRESS_FIELDS.postal).fill(CHAR_TEXT_11);
+            validateProceedDisabled();
+        }) // End step 'confirm character limits for fields'
+
+        // Address cannot be submitted if field if any field is missing
+        // Address can be submitted once fields are filled in
     })
-    // Address cannot be submitted if field if any field is missing
-    // Address can be submitted once fields are filled in
-
-})
-// End test case "Guest Checkout Flow"
+    // End test case "Guest Checkout Flow"
 
 }) // End Billing Address Test
